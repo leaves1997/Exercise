982d3058813b966dfec2247cc8fa445d43a9b25f
Leaves <chu.luo.luo@foxmail.com>
2019-07-19 18:45:40 +0800

    update

--- GXRCscrapy/GXRCscrapy/spiders/main_spider.py
+++ GXRCscrapy/GXRCscrapy/spiders/main_spider.py
@@ -1,7 +1,7 @@
 import scrapy
-from GXRCscrapy.items import GxrcItem
-from GXRCscrapy.items import Infolist
 from bs4 import BeautifulSoup
+import csv
+import os
 
 
 class GCRCscrapy(scrapy.Spider):
@@ -16,24 +16,21 @@ class GCRCscrapy(scrapy.Spider):
         html = response.text
         industinfo = BeautifulSoup(html, 'lxml').find_all('div', class_='tit')
         for i in industinfo:
-            industryTitle = i.find("a").get_text(strip=True)
             industryUrl = 'http://s.gxrc.com' + i.find("a").get('href')
-
-            info = GxrcItem()
-            info['Title'] = industryTitle
 
             yield scrapy.Request(url=industryUrl, callback=self.parse_list)
 
     def parse_list(self, response):
-        print(response.url)
-        print('开始爬取简历信息...')
         html = response.text
         page = BeautifulSoup(html, 'lxml')
+        Title = page.find(
+            'div', id='changeIndustry').find('h3').get_text(strip=True)
+        Title = Title.replace('/', '，')
+        print('开始爬取 %s 分类下的招聘信息...' % Title)
         nextPage = page.find('a', rel='next')
         infolist = page.find_all('div', class_='rlOne')
-
         for i in infolist:
-            info = Infolist()
+            info = {}
             info['position'] = i.find(
                 "a", class_='posName').get_text(strip=True)
             info['info_url'] = i.find("a", class_='posName').get('href')
@@ -47,12 +44,20 @@ class GCRCscrapy(scrapy.Spider):
             info['experience'] = i.find(
                 'ul', class_='qitaUL').find_all('li')[2].find('span').get_text(
                     strip=True)
-            yield info
+            info['date'] = i.find("li", class_='w5').get_text(strip=True)
+
+            f = open(Title + '.csv', 'a+', newline='')
+            writer = csv.writer(f, dialect='excel')
+            if os.path.getsize(Title + '.csv') == 0:
+                writer.writerow(
+                    ['职位', '链接', '公司', '薪水', '地址', '学历', '经验', '更新日期'])
+            writer.writerow(
+                (info['position'], info['info_url'], info['company'],
+                 info['salary'], info['site'], info['education'],
+                 info['experience'], info['date']))
+
+        print('保存信息完成...')
 
         if nextPage:
             next_url = 'http://s.gxrc.com' + nextPage.get('href')
-            print(next_url)
             return scrapy.Request(url=next_url, callback=self.parse_list)
-
-
-                
